423f987e30949db2f3645ab9f4ed80c8715d86af
Matheus Santos <matheussm301@gmail.com>
2020-12-18 23:10:32 -0300

    chore: add database destination

--- src/database/index.js
+++ src/database/index.js
@@ -1,8 +1,10 @@
 import low from 'lowdb';
-
+import path from 'path';
 import FileSync from 'lowdb/adapters/FileSync';
 
-const adapter = new FileSync('db.json');
+const databaseDestination = path.join(__dirname, 'db.json');
+
+const adapter = new FileSync(databaseDestination);
 
 const db = low(adapter);
 
