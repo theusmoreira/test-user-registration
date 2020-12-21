eea6123e7bfa8e1280d067947a5df7cf59628f3e
Matheus Santos <matheussm301@gmail.com>
2020-12-20 22:20:12 -0300

    feat: add static files, errors celebrate and urlencoded

--- src/server.js
+++ src/server.js
@@ -1,18 +1,23 @@
 import 'dotenv/config';
-
+import '@babel/polyfill/noConflict';
 import express from 'express';
 import logger from 'morgan';
-import AppError from './helpers/AppError';
+import path from 'path';
+import { errors } from 'celebrate';
 
+import AppError from './helpers/AppError';
 import routes from './routes';
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(logger('dev'));
 app.use(routes);
+app.use(errors());
 
-app.use((err, request, response, next) => {
+app.use((err, request, response, _) => {
   if (err instanceof AppError) {
     return response
       .status(err.statusCode)
@@ -29,5 +34,7 @@ app.use((err, request, response, next) => {
 const PORT = 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(
+    `Server running on port ${PORT} - http://localhost:${3000}/sign-in`,
+  );
 });
